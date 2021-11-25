fde4ef916fd522882b822384875adc24d91030f9
michael <87787413+cello-phane@users.noreply.github.com>
2021-11-25 12:41:58 -0800

    Update paginate_cells.py

--- paginate_cells.py
+++ paginate_cells.py
@@ -30,14 +30,17 @@ new_row = 0
 col_iter = 0
 for iter, n in enumerate(ranges_[::nth_rows*pages]):
     if divm_rows_uneven[1] > 0:
-        if n+nth_rows >= numberofpages*nth_rows+divm_rows_uneven[1]:
+        if iter > numberofpages-1:
             break
     for rownum in ranges_[n:n+nth_rows]:
         col_iter = 0
         row_values = [value for value in xlsfile_[rownum]]
         next_rown_after_=rownum+nth_rows
-        row_values_r = [value for value in xlsfile_[ranges_[next_rown_after_]]] 
-        row_values.extend(row_values_r)        
+        if next_rown_after_ < numberofpages*nth_rows:
+            row_values_r = [value for value in xlsfile_[ranges_[next_rown_after_]]] 
+            row_values.extend(row_values_r) 
+        else:
+            row_values_r = []
         for p in range(pages):
             for col in nth_cols.split(','):
                 colnum_ = int(col_to_n(str(col)))
@@ -48,12 +51,20 @@ for iter, n in enumerate(ranges_[::nth_rows*pages]):
                         writesheet.write(new_row, col_iter, str(row_values[colnum_+len(row_values_r)]))
                 col_iter+=1
         new_row+=1
-tail_start_row = new_row
-if divm_rows_uneven[1] > 0:
-    for n in ranges_[nth_rows*numberofpages:nth_rows*numberofpages+divm_rows_uneven[1]]:
-        row_values = [value for value in xlsfile_[ranges_[n]]]
-        for col in nth_cols.split(','):
-            colnum_ = int(col_to_n(str(col)))
-            writesheet.write(tail_start_row, colnum_, str(row_values[colnum_]))
-        tail_start_row+=1
+tail_start_row = new_row-nth_rows
+tail_col_start = len(nth_cols.split(','))
+endouterloop = False
+if divm_rows_uneven[1]:
+    if divm_rows_uneven[1] < nth_rows:
+        for enum,n in enumerate(ranges_[nth_rows*numberofpages:]):
+            col_tail = tail_col_start
+            if enum <= nth_rows:         
+                row_values = [value for value in xlsfile_[ranges_[n]]]
+                for col in nth_cols.split(','):
+                    colnum_ = int(col_to_n(str(col)))
+                    writesheet.write(tail_start_row, col_tail, str(row_values[colnum_]))
+                    col_tail+=1
+            else:
+                break
+            tail_start_row+=1
 writebook.save(writefile)
