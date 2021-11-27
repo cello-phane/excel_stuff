2c8f4272846f85995044455ad15f4670cd01f94f
michael <87787413+cello-phane@users.noreply.github.com>
2021-11-27 08:43:49 -0800

    Update paginate_cells.py

--- paginate_cells.py
+++ paginate_cells.py
@@ -8,35 +8,40 @@ xlspath = sys.path[0] + '\\'
 workbookfile = ''
 while not workbookfile or not path.exists(''.join([xlspath,workbookfile])):
     workbookfile = input('Excel File: ' + ' \nin this dir: ' + str(''.join([xlspath])))
-excelpath=str(xlspath)+str(workbookfile)        
-worksheet_number = input('Worksheet #:')
-if containsNumber(worksheet_number):
-    xlsfile_ = open_excel(to_read=excelpath,sheet_num=int(worksheet_number)-1)    
-else:
-    xlsfile_ = open_excel(to_read=excelpath)
+excelpath=str(xlspath)+str(workbookfile)
+##file to write after completion is using name of file for the sheet and name of file+"_new" for the filename
 writebook = xlwt.Workbook()
 writesheet = writebook.add_sheet(str(remove_ext(workbookfile)).strip('\\'), cell_overwrite_ok=True)
 writefile = xlspath + str(remove_ext(workbookfile)) + "_new.xls"
-ranges_= []
+
+cellranges_= []
+worksheet_number = input('Worksheet #:')
 nrowsinput = input('Select rows to paginate(leave blank for all):')
 if workbookfile.lower().endswith('xls'):
-    ranges_ = range_expand(input_r = str(nrowsinput), delim=',', first_n=0, last_n=len(xlsfile_))
+    if containsNumber(worksheet_number):
+        xlslen = get_xls_length(excelpath,sheet_num=int(worksheet_number)-1)
+        cellranges_ = cellranges(input_r = str(nrowsinput), delim=',', first_n=1, last_n=xlslen)
+        xlsfile_ = open_excel(to_read=excelpath,sheet_num=int(worksheet_number)-1,ranges=cellranges_)
+    else:
+        xlslen = get_xls_length(excelpath,sheet_num=0)
+        cellranges_ = cellranges(input_r = str(nrowsinput), delim=',', first_n=1, last_n=xlslen)
+        xlsfile_ = open_excel(to_read=excelpath,sheet_num=0,ranges=cellranges_)
 nth_rows = int(input('Number of rows on each subpage:'))
 nth_cols = input('Columns on each subpage(Ex. a,b,c,D,E,F):')
 pages = 2
-divm_rows_uneven = divmod(len(ranges_),int(nth_rows))
+divm_rows_uneven = divmod(len(cellranges_),int(nth_rows))
 numberofpages = divm_rows_uneven[0]#the number of pages that can be divided evenly into sections of nth_row rows
 new_row = 0
 col_iter = 0
 tail_start_row = 0
-for iter, n in enumerate(ranges_[::nth_rows*pages]):
-    for rownum in ranges_[n:n+nth_rows]:
+for iter, n in enumerate(cellranges_[::nth_rows*pages]):
+    for rownum in cellranges_[n:n+nth_rows]:
         col_iter = 0
         row_values = [value for value in xlsfile_[rownum]]
         next_rown_after_=rownum+nth_rows
         if next_rown_after_ < numberofpages*nth_rows:
-            row_values_r = [value for value in xlsfile_[ranges_[next_rown_after_]]] 
-            row_values.extend(row_values_r) 
+            row_values_r = [value for value in xlsfile_[cellranges_[next_rown_after_]]]
+            row_values.extend(row_values_r)
         else:
             row_values_r = []
         for p in range(pages):
@@ -49,10 +54,10 @@ for iter, n in enumerate(ranges_[::nth_rows*pages]):
                         writesheet.write(new_row, col_iter, str(row_values[colnum_+len(row_values_r)]))
                 col_iter+=1
         new_row+=1
-offset_ = (new_row - nth_rows * pages) + (nth_rows*2)+divm_rows_uneven[1]
+offset_ = (new_row - nth_rows * pages) + (nth_rows*2) + divm_rows_uneven[1]
 start_row = new_row - nth_rows
-if (divm_rows_uneven[1] + len(ranges_) - offset_) != nth_rows:
-    for rownum in ranges_[(numberofpages)*nth_rows:]:
+if (divm_rows_uneven[1] + len(cellranges_) - offset_) < nth_rows:
+    for rownum in cellranges_[(numberofpages)*nth_rows:]:
         col_iter = len(nth_cols.split(','))
         row_values = [value for value in xlsfile_[rownum]]
         for col in nth_cols.split(','):
